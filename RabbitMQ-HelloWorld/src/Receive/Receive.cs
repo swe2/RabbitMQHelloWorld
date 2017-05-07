186cc9c38c28acbe72c791de625ad6f09c0d5fc2
swe2 <swethat9234@gmail.com>
2017-05-07 09:12:08 -0400

    RedditMQ - Simple Hello World Project
    Has Two Projects 
    Send(Publisher)
    Receive(Consumer)

---
+++ RabbitMQ-HelloWorld/src/Receive/Receive.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receive
+{
+    public class Receive
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "helloWorld",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received Message {0}", message);
+                    };
+                    channel.BasicConsume(queue: "helloWorld", noAck: true, consumer: consumer);
+
+                    Console.WriteLine("Press enter to exit");
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ-HelloWorld/src/Send/Send.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    public class Send
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "helloWorld",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                    string message = "Hello World!";
+
+                    var messageBody = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "helloWorld",
+                                         basicProperties: null,
+                                         body: messageBody);
+
+                    Console.WriteLine(string.Format("Sending Message {0}", message));
+
+                    Console.WriteLine("Press enter to exit");
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
